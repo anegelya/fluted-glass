cd466e24623e05f3b284ba243fd629c3fdd7a219
Anatolii Nehelia <a.negelya@windowslive.com>
2023-12-21 14:22:55 +0200

    remove button

--- src/App.vue
+++ src/App.vue
@@ -3,14 +3,10 @@
     <h1 class="text-[6em] leading-none mb-[2em]">TOI-<br>3235 b</h1>
     <img src="/planet.png" alt="TOI-3235 b" class="relative w-screen mb-[20em]">
   </div>
-  <div class="fixed bottom-4 w-full h-[7em] p-4">
-    <FlutedGlass>
-      <CheckoutButton />
-    </FlutedGlass>
+        <span class="text-white">fluted glass</span>
   </div>
 </template>
 
 <script setup lang="ts">
 import FlutedGlass from './components/FlutedGlass.vue'
-import CheckoutButton from './components/CheckoutButton.vue'
 </script>
