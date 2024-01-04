a9719e066c35ce57fecd34ca3f4335f287a18e10
Anatolii Nehelia <a.negelya@windowslive.com>
2024-01-04 20:07:38 +0200

    style sup & sub elements

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="max-w-screen overflow-x-hidden py-24 text-[#ffd68a]">
     <p class="text-[18vw] leading-none break-words break-all">
-      TOI-3235 b. Mass (Mj) - 0.665±0.025, Radius (Rj) - 1.017±0.044, Period (days) 2.59261842±0.00000041, Semi-major axis (AU) - 0.02709±0.00046, Temp. (K) - 604±19.
+      TOI-3235 b. Mass (<i>M</i><sub>j</sub>) - 0.665±0.025, Radius (<i>R</i><sub>j</sub>) - 1.017±0.044, Period (days) 2.59261842±0.00000041, Semi-major axis (AU) - 0.02709±0.00046, Temp. (K) - 604±19.
     </p>
     <p class="text-[18vw] leading-none break-words break-all">
-      TOI-4127 b. Mass (Mj) - 2.30±0.11, Radius (Rj) - 1.096+0.039−0.032, Period (days) 56.39879±0.00010, Semi-major axis (AU) - 0.3081+0.0055−0.0058, Temp. (K) - 605.1+9.6−8.2.
+      TOI-4127 b. Mass (<i>M</i><sub>j</sub>) - 2.30±0.11, Radius (<i>R</i><sub>j</sub>) - 1.096+0.039−0.032, Period (days) 56.39879±0.00010, Semi-major axis (AU) - 0.3081<sup>+0.0055</sup><sub>−0.0058</sub>, Temp. (K) - 605.1<sup>+9.6</sup><sub>−8.2</sub>.
     </p>
   </div>
   <div class="fixed top-0 left-0 right-0 bottom-0 w-full h-svh flex justify-center items-center z-10">
